4a0bc97e8582b8e21e86dc064c15c3678a0618ac
DonteCarter11 <dontecarter269@gmail.com>
2025-06-26 18:44:21 -0400

    added search bar and slider

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // API FOR DIGIMON https://digimon-api.vercel.app/api/digimon
 
+const slider = document.querySelector(".range-slider");
+const progress = slider.querySelector(".progress");
+const minPriceInput = slider.querySelector(".min-price");
+const maxPriceInput = slider.querySelector(".max-price");
+const minInput = slider.querySelector(".min-input");
+const maxInput = slider.querySelector(".max-input");
+
